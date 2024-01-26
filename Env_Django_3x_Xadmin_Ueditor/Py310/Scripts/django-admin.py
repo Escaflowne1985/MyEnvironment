6239f31418230ad64ecdfec0fa9e47595ddc51d5
Escaflowne1985 <33034782@qq.com>
2024-01-26 19:52:54 +0800

    2024-01-26

---
+++ Env_Django_3x_Xadmin_Ueditor/Py310/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!H:\MyGitProject\DjangoProject\_Env_Django_3x\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ README.md
@@ -0,0 +1,8 @@
+# Django3-Py36-deployment-configuration
+
+Python3.10 
+Django3.0.3
+
+已经适配 Django Ueditor 和 xadmin 适配。
+
+通用基础环境配置和项目开发项目配置。
